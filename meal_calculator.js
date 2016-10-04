f5fce8781df646ed7ca50f44d55ed61948209187
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2016-10-04 13:45:30 -0500

    created objects and methods

---
+++ meal_calculator.js
@@ -0,0 +1,73 @@
+// Object constructor for diners
+var Diner = function(name,dishes){
+	this.name = name;
+	this.dishes = dishes;
+}
+// Method to calculate total bill of dishes consumed
+Diner.prototype.isTotalOfDishes = function(dishes){
+	var dinerTotal = 0;
+  	for( var key in dishes) {
+    	if( dishes.hasOwnProperty(key)) {
+      		dinerTotal += parseFloat(dishes[key]);
+    	}
+  	}
+  	return dinerTotal;
+}
+// Method to calculate the tax for the total
+Diner.prototype.isTaxForDiner = function(total){
+	return (total * 0.08) 
+}
+// Method to calculate the tip 
+Diner.prototype.isTipForDiner = function(total){
+	return (total * 0.20) 
+}
+
+// Object constructor for the bill
+var Bill = function(diners){
+	this.diners = diners;
+}
+
+Bill.prototype.isTotalBill = function(diners){
+	var billTotalArray = diners.map(function(item){
+		return (item.isTotalOfDishes(item.dishes)) + (item.isTaxForDiner(item.isTotalOfDishes(item.dishes)))
+	})
+	var billTotal = billTotalArray.reduce((a, b) => a + b, 0);
+	return billTotal
+}
+
+Bill.prototype.isTotalTips = function(diners){
+	var totalTipArray = diners.map(function(item){
+		return (item.isTipForDiner(item.isTotalOfDishes(item.dishes)))
+	})
+	var tipTotal = totalTipArray.reduce((a, b) => a + b, 0);
+	return tipTotal
+}
+
+Bill.prototype.isBillBreakdown = function(diners){
+	var billBreakdown = diners.map(function(item){
+		item["dinerTotal"] = (item.isTotalOfDishes(item.dishes)) + (item.isTaxForDiner(item.isTotalOfDishes(item.dishes)))
+		item["dinerTip"] = item.isTipForDiner(item.isTotalOfDishes(item.dishes))
+		return diners
+	})
+	return diners
+}
+
+
+// Diner objects
+var diner1 = Object.create(Diner.prototype)
+diner1.name = "Maria"
+diner1.dishes = {soup:7,pasta:15}
+var diner2 = Object.create(Diner.prototype)
+diner2.name = "Marti"
+diner2.dishes = {soup:7, meatDish:30}
+var diner3 = Object.create(Diner.prototype)
+diner3.name = "Ana"
+diner3.dishes = {salad:15, cake:7}
+
+// Bill object
+var billForDiners = Object.create(Bill.prototype)
+billForDiners.diners = [diner1,diner2,diner3]
+billForDiners.isTotalBill(billForDiners.diners)
+billForDiners.isTotalTips(billForDiners.diners)
+billForDiners.isBillBreakdown(billForDiners.diners)
+
