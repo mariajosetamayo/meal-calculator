286aac2524747f681a77a7186fc71618c620b060
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2016-10-06 12:43:54 -0600

    refactored code using ES6

--- meal_calculator.js
+++ meal_calculator.js
@@ -1,72 +1,81 @@
-// Object constructor for diners
-var Diner = function(name,dishes){
-	this.name = name;
-	this.dishes = dishes;
-}
-// Method to calculate total bill of dishes consumed
-Diner.prototype.isTotalOfDishes = function(dishes){
-	var dinerTotal = 0;
-  	for( var key in dishes) {
-    	if( dishes.hasOwnProperty(key)) {
-      		dinerTotal += parseFloat(dishes[key]);
-    	}
-  	}
-  	return dinerTotal;
-}
-// Method to calculate the tax for the total
-Diner.prototype.isTaxForDiner = function(total){
-	return (total * 0.08) 
-}
-// Method to calculate the tip 
-Diner.prototype.isTipForDiner = function(total){
-	return (total * 0.20) 
-}
+"use strict"; 
+// Object constructor for a Person
+class Person {
+	constructor(name) {
+		this.name = name;
+	}
+};
+// This class inherits from the object Person and creates a new constructor Diner
+class Diner extends Person {
+	// Constructor for diners
+	constructor(name, dishes) {
+		super(name);
+		this.dishes = dishes;
+	}
+	// Method to calculate total bill of dishes consumed
+	totalPriceOfDishes(dishes) {
+		let dinerTotal = 0;
+  		for( var key in dishes) {
+    		if( dishes.hasOwnProperty(key)) {
+      			dinerTotal += parseFloat(dishes[key]);
+    		}
+  		}
+  		return dinerTotal;
+	}
+	// Method to calculate the tax for the total
+	taxForDiner(total) {
+		return (total * 0.08); 
+	}
+	// Method to calculate the tip
+	tipForDiner(total) {
+		return (total * 0.20); 
+	}
+};
 
-// Object constructor for the bill
-var Bill = function(diners){
-	this.diners = diners;
-}
-//Method to calculate the total bill
-Bill.prototype.isTotalBill = function(diners){
-	var billTotalArray = diners.map(function(item){
-		return (item.isTotalOfDishes(item.dishes)) + (item.isTaxForDiner(item.isTotalOfDishes(item.dishes)))
-	})
-	var billTotal = billTotalArray.reduce((a, b) => a + b, 0);
-	return billTotal
-}
-// Method to calculate the total tips
-Bill.prototype.isTotalTips = function(diners){
-	var totalTipArray = diners.map(function(item){
-		return (item.isTipForDiner(item.isTotalOfDishes(item.dishes)))
-	})
-	var tipTotal = totalTipArray.reduce((a, b) => a + b, 0);
-	return tipTotal
-}
-// Method to breakdown the bill among diners
-Bill.prototype.isBillBreakdown = function(diners){
-	var billBreakdown = diners.forEach(function(item){
-		item["dinerTotal"] = (item.isTotalOfDishes(item.dishes)) + (item.isTaxForDiner(item.isTotalOfDishes(item.dishes)))
-		item["dinerTip"] = item.isTipForDiner(item.isTotalOfDishes(item.dishes))
-	})
-	return diners
-}
+class Bill {
+	// Constructor for bill
+	constructor(diners) {
+		this.diners = diners;
+	}
+	// Method to calculate the total bill
+	totalBill(diners) {
+		let billTotalArray = diners.map((item) => {
+			return (item.totalPriceOfDishes(item.dishes)) + (item.taxForDiner(item.totalPriceOfDishes(item.dishes)));
+		});
+		let billTotal = billTotalArray.reduce((a, b) => a + b, 0);
+		return billTotal;
+	}
+	// Method to calculate the total tips
+	totalTips(diners) {
+		let totalTipArray = diners.map((item) => {
+			return (item.tipForDiner(item.totalPriceOfDishes(item.dishes)));
+		});
+		let tipTotal = totalTipArray.reduce((a, b) => a + b, 0);
+		return tipTotal;
+	}
+	// Method to breakdown the bill among diners
+	billBreakdown(diners) {
+		let billBreakdown = diners.forEach((item) => {
+			item["dinerTotal"] = (item.totalPriceOfDishes(item.dishes)) + (item.taxForDiner(item.totalPriceOfDishes(item.dishes)));
+			item["dinerTip"] = item.tipForDiner(item.totalPriceOfDishes(item.dishes));
+		});
+		return diners;
+	}
+};
 
 // Diner objects
-var diner1 = Object.create(Diner.prototype)
-diner1.name = "Maria"
-diner1.dishes = {soup:7,pasta:15}
-var diner2 = Object.create(Diner.prototype)
-diner2.name = "Marti"
-diner2.dishes = {soup:7, meatDish:30}
-var diner3 = Object.create(Diner.prototype)
-diner3.name = "Ana"
-diner3.dishes = {salad:15, cake:7}
-
+const diner1 = new Diner("Maria", {soup:7,pasta:15});
+const diner2 = new Diner("Marti", {soup:7, meatDish:30});
+const diner3 = new Diner("Ana", {salad:15, cake:7});
 // Bill object
-var billForDiners = Object.create(Bill.prototype)
-billForDiners.diners = [diner1,diner2,diner3]
+const billForDiners = new Bill([diner1,diner2,diner3]);
 // Variables containing the total bill, the total tip and the diners'bill breakdown
-var totalBill = billForDiners.isTotalBill(billForDiners.diners)
-var totalTip = billForDiners.isTotalTips(billForDiners.diners)
-var dinersBillBreakdown = billForDiners.isBillBreakdown(billForDiners.diners)
+const totalBill = billForDiners.totalBill(billForDiners.diners);
+const totalTip = billForDiners.totalTips(billForDiners.diners);
+const dinersBillBreakdown = billForDiners.billBreakdown(billForDiners.diners);
+// Console logs to run code in node
+console.log("total bill: " + totalBill);
+console.log("total tip: " + totalTip);
+console.log(dinersBillBreakdown);
+
 
